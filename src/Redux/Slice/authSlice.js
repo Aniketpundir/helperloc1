255eeccf47b65442f4883d19d09809e63ec91410
Aniket Kumar <aniketpundir348@gmail.com>
2026-06-10 18:32:05 +0530

    Console somethimg

--- src/Redux/Slice/authSlice.js
+++ src/Redux/Slice/authSlice.js
@@ -136,7 +136,7 @@ const authSlice = createSlice({
                 state.error = null;
             })
             .addCase(registerUser.fulfilled, (state, action) => {
-                console.log('Login Response:', action.payload); /
+                console.log('Login Response:', action.payload);
                 const { token, user } = action.payload;
                 state.loading = false;
                 state.token = token;
